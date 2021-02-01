e619344ad24242624afed1837ca99da0550a263e
Adrien <Adrien@info80.fr>
2021-02-01 21:40:51 +0100

    Update to get Blues fix

--- server/main.lua
+++ server/main.lua
@@ -1,10 +1,11 @@
 local VorpCore = {}
 
-TriggerEvent("getCore", function(core)
+TriggerEvent("getCore",function(core)
     VorpCore = core
 end)
 
 VorpInv = exports.vorp_inventory:vorp_inventoryApi()
+
 
 RegisterServerEvent("syn_death:delete")
 AddEventHandler("syn_death:delete", function()
@@ -14,28 +15,30 @@ AddEventHandler("syn_death:delete", function()
     local gold = Character.gold
     local role = Character.rol
 
-    if Config.removeweapons then
+    if Config.removeweapons then 
         TriggerEvent("vorpCore:getUserWeapons", tonumber(_source), function(getUserWeapons)
-            for k, v in pairs(getUserWeapons) do
-                VorpInv.subWeapon(_source, v.id)
-            end
+           for k, v in pairs (getUserWeapons) do
+            local id = v.id
+            VorpInv.subWeapon(_source, v.id)
+            exports.ghmattimysql:execute("DELETE FROM loadout WHERE id=@id", { ['id'] = id})
+           end
         end)
     end
-    if Config.removeitems then
+    if Config.removeitems then 
         TriggerEvent("vorpCore:getUserInventory", tonumber(_source), function(getInventory)
-            for k, v in pairs(getInventory) do
-                VorpInv.subItem(_source, v.name, v.count)
+            for k, v in pairs (getInventory) do
+                VorpInv.subItem(_source, v.name, v.count)  
             end
-        end)
+        end) 
     end
 
-    if Config.removecash then
+    if Config.removecash then 
         Character.removeCurrency(0, money)
     end
-    if Config.removegold then
+    if Config.removegold then 
         Character.removeCurrency(1, gold)
     end
-    if Config.removerole then
+    if Config.removerole then 
         Character.removeCurrency(2, role)
     end
-end)
+end) 
